7436d4bde6f928a95f590af45247d34dfd449fcf
Wenley Tong <wenley.tong@gmail.com>
2020-08-05 23:07:41 -0700

    Simplify skeleton types

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,6 @@
-
-struct Pattern;
-
-impl Pattern {
-    pub fn matches(record: String) -> bool {
-        true
-    }
+pub struct Program {
 }
 
-struct Command;
-
-impl Command {
-    pub fn execute(record: String) {}
-}
-
-struct Rule {
-    pattern: Pattern,
-    command: Command,
-}
-
-struct Program {
-    rules: Vec<Rule>,
-}
-
-pub trait Runnable {
-}
-
-impl Runnable for Program {
-}
-
-pub fn parse_program(program_text: String) -> Box<dyn Runnable> {
-    Box::new(Program { rules: vec![] })
+pub fn parse_program(_program_text: String) -> Program {
+    Program {}
 }
