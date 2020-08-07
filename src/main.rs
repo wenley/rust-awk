2fdde8d51622f1dabf2a7a4df3152596fbf1c65a
Wenley Tong <wenley.tong@gmail.com>
2020-08-06 21:17:20 -0700

    v0.1 Repeat stdin to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 
 use std::env;
+use std::io;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     println!("Hello, world!");
     println!("args = {:?}", args);
+
+    let stdin = io::stdin();
+
+    let mut buffer = String::new();
+    loop {
+        match stdin.read_line(&mut buffer) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                buffer.truncate(n - 1);
+                println!("{}", buffer);
+                buffer.clear();
+            }
+            Err(error) => {
+                eprintln!("Error encountered: {}", error);
+            }
+        }
+    }
 }
