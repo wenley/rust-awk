cb2e8fbaaf48004c5e0888d8818c6d1bf2aa0107
Wenley Tong <wenley.tong@gmail.com>
2020-08-06 21:06:21 -0700

    Separate program execution from program parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,18 @@ pub struct Program {
 pub fn parse_program(_program_text: String) -> Program {
     Program {}
 }
+
+pub struct Context {
+}
+
+pub struct ProgramRun<'a> {
+    program: &'a Program,
+    context: Context,
+}
+
+pub fn start_run<'a>(program: &'a Program) -> ProgramRun<'a> {
+    ProgramRun {
+        program: program,
+        context: Context {},
+    }
+}
