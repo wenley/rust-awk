4fd5c7610b80c9befdde475b7583bbc73e4f5aa9
Wenley Tong <wenley.tong@gmail.com>
2020-08-15 13:17:36 -0700

    Add binary

--- src/expression.rs
+++ src/expression.rs
@@ -1,8 +1,10 @@
 use regex::Regex;
 
+#[derive(Debug)]
 pub enum Expression {
     StringLiteral(String),
     NumericLiteral(NumericValue),
+    AddBinary { left: Box<Expression>, right: Box<Expression> },
 }
 
 #[derive(PartialEq,Debug,Clone)]
@@ -22,6 +24,12 @@ impl Expression {
         match self {
             Expression::StringLiteral(string) => { Value::String(string.clone()) }
             Expression::NumericLiteral(numeric) => { Value::Numeric(numeric.clone()) }
+            Expression::AddBinary { left: left, right: right } => {
+                match (left.evaluate(), right.evaluate()) {
+                    (Value::Numeric(NumericValue::Integer(x)), Value::Numeric(NumericValue::Integer(y))) => { Value::Numeric(NumericValue::Integer(x + y)) }
+                    _ => { panic!("Unsupported addition values {:?} and {:?}", left, right) }
+                }
+            }
         }
     }
 }
@@ -38,5 +46,16 @@ mod tests {
 
     #[test]
     fn variables_can_evaluate() {
+    }
+
+    #[test]
+    fn binary_expressions_can_evaluate() {
+        assert_eq!(
+            Expression::AddBinary {
+                left: Box::new(Expression::NumericLiteral(NumericValue::Integer(2))),
+                right: Box::new(Expression::NumericLiteral(NumericValue::Integer(3))),
+            }.evaluate(),
+            Value::Numeric(NumericValue::Integer(5)),
+        );
     }
 }
