244bbb90bee2a963c0c9b183c78b3f9ccf701e11
Wenley Tong <wenley.tong@gmail.com>
2020-08-16 00:54:10 -0700

    Implement basic control flow

--- src/item.rs
+++ src/item.rs
@@ -4,7 +4,30 @@ use crate::expression::Expression;
 static EMPTY_STRING: &str = "";
 
 pub enum Statement {
+    IfElse {
+        condition: Expression,
+        if_branch: Box<Statement>,
+        else_branch: Box<Statement>
+    },
     Print(Expression),
+}
+
+impl Statement {
+    pub fn evaluate(&self, context: &basic_types::Context) -> String {
+        match self {
+            Statement::Print(expression) => {
+                expression.evaluate(context).coerce_to_string()
+            }
+            Statement::IfElse { condition, if_branch, else_branch } => {
+                let result = condition.evaluate(context).coercion_to_boolean();
+                if result {
+                    if_branch.evaluate(context)
+                } else {
+                    else_branch.evaluate(context)
+                }
+            }
+        }
+    }
 }
 
 pub struct Action {
@@ -19,9 +42,7 @@ impl Action {
     ) -> Vec<String> {
         self.statements
             .iter()
-            .map(|statement| match statement {
-                Statement::Print(expression) => expression.evaluate(context).coerce_to_string(),
-            })
+            .map(|statement| statement.evaluate(context))
             .collect()
     }
 }
@@ -45,4 +66,44 @@ impl Pattern {
 pub struct Item {
     pub pattern: Pattern,
     pub action: Action,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_statement_produces_value() {
+        let empty_context = basic_types::Context::empty();
+        let print_statement = Statement::Print(Expression::StringLiteral("hello".to_string()));
+        assert_eq!(
+            print_statement.evaluate(&empty_context),
+            "hello",
+        );
+    }
+
+    #[test]
+    fn if_produces_correct_value() {
+        let empty_context = basic_types::Context::empty();
+
+        let if_conditional = Statement::IfElse {
+            condition: Expression::StringLiteral("not empty".to_string()),
+            if_branch: Box::new(Statement::Print(Expression::StringLiteral("if-branch".to_string()))),
+            else_branch: Box::new(Statement::Print(Expression::StringLiteral("else".to_string()))),
+        };
+        assert_eq!(
+            if_conditional.evaluate(&empty_context),
+            "if-branch",
+        );
+
+        let else_conditional = Statement::IfElse {
+            condition: Expression::StringLiteral("".to_string()),
+            if_branch: Box::new(Statement::Print(Expression::StringLiteral("if-branch".to_string()))),
+            else_branch: Box::new(Statement::Print(Expression::StringLiteral("else".to_string()))),
+        };
+        assert_eq!(
+            else_conditional.evaluate(&empty_context),
+            "else",
+        );
+    }
 }
