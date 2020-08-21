6141c4beccb089c8c30610270ad4c28c0b9a6a36
Wenley Tong <wenley.tong@gmail.com>
2020-08-20 23:10:23 -0700

    Add parser for string literals

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,13 @@
+extern crate nom;
+
+use nom::{
+    IResult,
+    character::complete::{
+        one_of,
+        alpha1,
+    },
+};
+
 use crate::{
     expression::Expression,
     item
@@ -7,16 +17,38 @@ pub struct Program {
     pub items: Vec<item::Item>,
 }
 
-pub fn parse_program(_program_text: &str) -> Program {
-    Program {
+fn parse_string_literal(input: &str) -> IResult<&str, Expression> {
+    let (input, _) = one_of("\"")(input)?;
+    let (input, contents) = alpha1(input)?;
+    let (input, _) = one_of("\"")(input)?;
+
+    IResult::Ok((input, Expression::StringLiteral(contents.to_string())))
+}
+
+pub fn parse_program(program_text: &str) -> Program {
+    let default_program = Program {
         items: vec![item::Item {
             pattern: item::Pattern::MatchEverything,
             action: item::Action {
                 statements: vec![item::Statement::Print(
-                    Expression::StringLiteral("hi".to_string()),
-                )],
+                                Expression::StringLiteral("hi".to_string()),
+                                )],
             },
         }],
+    };
+
+    match parse_string_literal(program_text) {
+        Ok((_, expr)) => {
+            Program {
+                items: vec![item::Item {
+                    pattern: item::Pattern::MatchEverything,
+                    action: item::Action {
+                        statements: vec![item::Statement::Print(expr)],
+                    },
+                }]
+            }
+        }
+        _ => { default_program }
     }
 }
 
