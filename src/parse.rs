29e57aacd03bf1a948f54070c17c58b0540a6e07
Wenley Tong <wenley.tong@gmail.com>
2020-08-22 21:59:57 -0700

    Apply parsing techniques for recursion
    Best practices gleaned from
    https://craftinginterpreters.com/parsing-expressions.html

--- src/parse.rs
+++ src/parse.rs
@@ -6,10 +6,10 @@ use crate::basic_types::NumericValue;
 use nom::{
     branch::alt,
     character::complete::{alpha1, multispace0, none_of, one_of},
-    combinator::{all_consuming, map, map_res},
-    multi::many1,
+    combinator::{map, map_res},
+    multi::{many0, many1},
     re_find,
-    sequence::{delimited, separated_pair, tuple},
+    sequence::{delimited, pair, tuple},
     IResult,
 };
 
@@ -19,26 +19,41 @@ pub struct Program {
     pub items: Vec<item::Item>,
 }
 
+/* - - - - - - - - - -
+ * Expression Parsers
+ * in increasing order of precedence
+ * - - - - - - - - - - */
+
 fn parse_expression(input: &str) -> IResult<&str, Expression> {
-    alt((parse_addition, parse_literal, parse_variable))(input)
+    parse_addition(input)
+}
+
+fn parse_addition(input: &str) -> IResult<&str, Expression> {
+    let parse_added_expr = map(
+        pair(delimited(multispace0, one_of("+"), multispace0), parse_primary),
+        |(_, rhs)| rhs
+    );
+    map(
+        pair(parse_primary, many0(parse_added_expr)),
+        move |(first, mut rest)| rest.drain(0..).fold(first, |inner, next| {
+            Expression::AddBinary {
+                left: Box::new(inner),
+                right: Box::new(next),
+            }
+        })
+    )(input)
+}
+
+fn parse_primary(input: &str) -> IResult<&str, Expression> {
+    alt((parse_literal, parse_variable, parse_parens))(input)
+}
+
+fn parse_parens(input: &str) -> IResult<&str, Expression> {
+    delimited(one_of("("), parse_expression, one_of(")"))(input)
 }
 
 fn parse_variable(input: &str) -> IResult<&str, Expression> {
     map(alpha1, |name: &str| Expression::Variable(name.to_string()))(input)
-}
-
-fn parse_addition(input: &str) -> IResult<&str, Expression> {
-    map(
-        separated_pair(
-            parse_expression,
-            delimited(multispace0, one_of("+"), multispace0),
-            parse_expression,
-        ),
-        |(left, right)| Expression::AddBinary {
-            left: Box::new(left),
-            right: Box::new(right),
-        },
-    )(input)
 }
 
 fn parse_literal(input: &str) -> IResult<&str, Expression> {
@@ -161,5 +176,29 @@ mod tests {
         assert_eq!(parse_float_literal("+e").is_err(), true);
 
         // Cannot test parse_number_literal because macros don't reveal types
+    }
+
+    #[test]
+    fn parse_expressions() {
+        let result = parse_expression("1");
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().1, Expression::NumericLiteral(NumericValue::Integer(1)));
+
+        let result = parse_expression("(1)");
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().1, Expression::NumericLiteral(NumericValue::Integer(1)));
+
+        let result = parse_expression("(1) + (2.5)");
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().1, Expression::AddBinary {
+            left: Box::new(Expression::NumericLiteral(NumericValue::Integer(1))),
+            right: Box::new(Expression::NumericLiteral(NumericValue::Float(2.5))),
+        });
+        let result = parse_expression("(1) + (2.5)");
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().1, Expression::AddBinary {
+            left: Box::new(Expression::NumericLiteral(NumericValue::Integer(1))),
+            right: Box::new(Expression::NumericLiteral(NumericValue::Float(2.5))),
+        });
     }
 }
