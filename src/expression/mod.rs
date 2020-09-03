057353a6c9bacc09787e9ba8d2eeb96fe233838e
Wenley Tong <wenley.tong@gmail.com>
2020-09-02 20:29:09 -0700

    Extract RegexMatch to nested module

--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -3,9 +3,8 @@ use std::fmt::Debug;
 
 use nom::{
     branch::alt,
-    bytes::complete::tag,
-    character::complete::{multispace0, one_of},
-    sequence::{delimited, preceded, tuple},
+    character::complete::one_of,
+    sequence::{delimited, preceded},
     IResult,
 };
 
@@ -16,6 +15,7 @@ use crate::{
 
 mod binary_math;
 mod literal;
+mod regex_match;
 mod variable;
 
 pub(crate) trait Expression: Debug {
@@ -27,11 +27,6 @@ pub(crate) trait Expression: Debug {
 #[derive(Debug)]
 enum ExpressionImpl {
     FieldReference(Box<dyn Expression>),
-    RegexMatch {
-        left: Box<dyn Expression>,
-        right: Box<dyn Expression>,
-        negated: bool,
-    },
 }
 
 impl Expression for ExpressionImpl {
@@ -57,24 +52,6 @@ impl Expression for ExpressionImpl {
                         .unwrap_or(Value::Uninitialized),
                 }
             }
-            ExpressionImpl::RegexMatch {
-                left,
-                right,
-                negated,
-            } => {
-                let left_value = left.evaluate(context, record).coerce_to_string();
-
-                let matches = match right.regex() {
-                    Some(r) => r.is_match(&left_value),
-                    None => {
-                        let right_value = right.evaluate(context, record).coerce_to_string();
-                        Regex::new(&right_value).unwrap().is_match(&left_value)
-                    }
-                };
-                let int_value = if matches ^ negated { 1 } else { 0 };
-
-                Value::Numeric(NumericValue::Integer(int_value))
-            }
         }
     }
 }
@@ -86,30 +63,7 @@ impl Expression for ExpressionImpl {
 /// until we reach literals and the parenthesized expressions.
 
 pub(crate) fn parse_expression(input: &str) -> IResult<&str, Box<dyn Expression>> {
-    alt((parse_regex_match, binary_math::parse_addition))(input)
-}
-
-// Regex matching does not associate
-fn parse_regex_match(input: &str) -> IResult<&str, Box<dyn Expression>> {
-    let (i, (left, operator, right)) = tuple((
-        binary_math::parse_addition,
-        delimited(multispace0, alt((tag("~"), tag("!~"))), multispace0),
-        binary_math::parse_addition,
-    ))(input)?;
-
-    let negated = match operator.len() {
-        1 => false,
-        2 => true,
-        _ => panic!("Unexpected regex operator length: {}", operator),
-    };
-    Result::Ok((
-        i,
-        Box::new(ExpressionImpl::RegexMatch {
-            left: left,
-            right: right,
-            negated,
-        }),
-    ))
+    alt((regex_match::parse_regex_match, binary_math::parse_addition))(input)
 }
 
 fn parse_primary(input: &str) -> IResult<&str, Box<dyn Expression>> {
@@ -188,41 +142,6 @@ mod tests {
         assert_eq!(
             expression.evaluate(&context, &record),
             Value::String("hello".to_string()),
-        );
-    }
-
-    #[test]
-    fn test_regex_match() {
-        let (context, record) = empty_context_and_record();
-
-        let result = parse_expression("1 ~ 2");
-        assert!(result.is_ok());
-        let expression = result.unwrap().1;
-        assert_eq!(
-            expression.evaluate(&context, &record),
-            Value::Numeric(NumericValue::Integer(0)),
-        );
-
-        // Cannot consume the full expression
-        let result = parse_expression("1 ~ 2 ~ 3");
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().0, " ~ 3");
-
-        let result = parse_expression("1 + 2 ~ 3");
-        assert!(result.is_ok());
-        let (remainder, expression) = result.unwrap();
-        assert_eq!(remainder, "");
-        assert_eq!(
-            expression.evaluate(&context, &record),
-            Value::Numeric(NumericValue::Integer(1)),
-        );
-
-        let result = parse_expression("1 !~ 2");
-        assert!(result.is_ok());
-        let expression = result.unwrap().1;
-        assert_eq!(
-            expression.evaluate(&context, &record),
-            Value::Numeric(NumericValue::Integer(1)),
         );
     }
 }
---
+++ src/expression/regex_match.rs
@@ -0,0 +1,117 @@
+use regex::Regex;
+use std::fmt::Debug;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::multispace0,
+    sequence::{delimited, tuple},
+    IResult,
+};
+
+use super::Expression;
+use crate::{
+    basic_types::{Context, Record},
+    value::{NumericValue, Value},
+};
+
+#[derive(Debug)]
+struct RegexMatch {
+    left: Box<dyn Expression>,
+    right: Box<dyn Expression>,
+    negated: bool,
+}
+
+impl Expression for RegexMatch {
+    fn regex<'a>(&'a self) -> Option<&'a Regex> {
+        None
+    }
+
+    fn evaluate<'a>(&self, context: &Context, record: &'a Record) -> Value {
+        let left_value = self.left.evaluate(context, record).coerce_to_string();
+
+        let matches = match self.right.regex() {
+            Some(r) => r.is_match(&left_value),
+            None => {
+                let right_value = self.right.evaluate(context, record).coerce_to_string();
+                Regex::new(&right_value).unwrap().is_match(&left_value)
+            }
+        };
+        let int_value = if matches ^ self.negated { 1 } else { 0 };
+
+        Value::Numeric(NumericValue::Integer(int_value))
+    }
+}
+
+// Regex matching does not associate
+pub(super) fn parse_regex_match(input: &str) -> IResult<&str, Box<dyn Expression>> {
+    let (i, (left, operator, right)) = tuple((
+        super::binary_math::parse_addition,
+        delimited(multispace0, alt((tag("~"), tag("!~"))), multispace0),
+        super::binary_math::parse_addition,
+    ))(input)?;
+
+    let negated = match operator.len() {
+        1 => false,
+        2 => true,
+        _ => panic!("Unexpected regex operator length: {}", operator),
+    };
+    Result::Ok((
+        i,
+        Box::new(RegexMatch {
+            left: left,
+            right: right,
+            negated,
+        }),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn empty_context_and_record() -> (Context, Record<'static>) {
+        (
+            Context::empty(),
+            Record {
+                full_line: "",
+                fields: vec![],
+            },
+        )
+    }
+
+    #[test]
+    fn test_regex_match() {
+        let (context, record) = empty_context_and_record();
+
+        let result = parse_regex_match("1 ~ 2");
+        assert!(result.is_ok());
+        let expression = result.unwrap().1;
+        assert_eq!(
+            expression.evaluate(&context, &record),
+            Value::Numeric(NumericValue::Integer(0)),
+        );
+
+        // Cannot consume the full expression
+        let result = parse_regex_match("1 ~ 2 ~ 3");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().0, " ~ 3");
+
+        let result = parse_regex_match("1 + 2 ~ 3");
+        assert!(result.is_ok());
+        let (remainder, expression) = result.unwrap();
+        assert_eq!(remainder, "");
+        assert_eq!(
+            expression.evaluate(&context, &record),
+            Value::Numeric(NumericValue::Integer(1)),
+        );
+
+        let result = parse_regex_match("1 !~ 2");
+        assert!(result.is_ok());
+        let expression = result.unwrap().1;
+        assert_eq!(
+            expression.evaluate(&context, &record),
+            Value::Numeric(NumericValue::Integer(1)),
+        );
+    }
+}
