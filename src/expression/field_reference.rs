ff3f653073ad6df5e4a368dc4a83a0e870ac35fa
Wenley Tong <wenley.tong@gmail.com>
2020-09-02 21:05:35 -0700

    Extract FieldReference to separate module

---
+++ src/expression/field_reference.rs
@@ -0,0 +1,91 @@
+use regex::Regex;
+use std::fmt::Debug;
+
+use nom::{character::complete::one_of, sequence::preceded, IResult};
+
+use super::Expression;
+use crate::{
+    basic_types::{Context, Record},
+    value::{NumericValue, Value},
+};
+
+#[derive(Debug)]
+struct FieldReference {
+    expression: Box<dyn Expression>,
+}
+
+impl Expression for FieldReference {
+    fn regex<'a>(&'a self) -> Option<&'a Regex> {
+        None
+    }
+
+    fn evaluate<'a>(&self, context: &Context, record: &'a Record) -> Value {
+        let value = self
+            .expression
+            .evaluate(context, record)
+            .coerce_to_numeric();
+        let unsafe_index = match value {
+            NumericValue::Integer(i) => i,
+            NumericValue::Float(f) => f.floor() as i64,
+        };
+        match unsafe_index {
+            i if i < 0 => panic!("Field indexes cannot be negative: {}", unsafe_index),
+            i if i == 0 => Value::String(record.full_line.to_string()),
+            i => record
+                .fields
+                .get((i - 1) as usize)
+                .map(|s| Value::String(s.to_string()))
+                .unwrap_or(Value::Uninitialized),
+        }
+    }
+}
+
+pub(super) fn parse_field_reference(input: &str) -> IResult<&str, Box<dyn Expression>> {
+    let (i, expr) = preceded(one_of("$"), super::parse_expression)(input)?;
+    Result::Ok((i, Box::new(FieldReference { expression: expr })))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::literal::*;
+    use super::*;
+
+    fn empty_context_and_record() -> (Context, Record<'static>) {
+        (
+            Context::empty(),
+            Record {
+                full_line: "",
+                fields: vec![],
+            },
+        )
+    }
+
+    #[test]
+    fn field_reference_can_evaluate() {
+        let (context, mut record) = empty_context_and_record();
+        record.fields = vec!["first", "second"];
+
+        assert_eq!(
+            FieldReference {
+                expression: Box::new(Literal::Numeric(NumericValue::Integer(1)))
+            }
+            .evaluate(&context, &record),
+            Value::String("first".to_string()),
+        );
+    }
+
+    #[test]
+    fn test_parse_field_reference() {
+        let (context, mut record) = empty_context_and_record();
+        let result = parse_field_reference("$1");
+        assert_eq!(result.is_ok(), true);
+        let expression = result.unwrap().1;
+        assert_eq!(expression.evaluate(&context, &record), Value::Uninitialized,);
+
+        record.fields = vec!["hello"];
+        assert_eq!(
+            expression.evaluate(&context, &record),
+            Value::String("hello".to_string()),
+        );
+    }
+}
--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -1,19 +1,15 @@
 use regex::Regex;
 use std::fmt::Debug;
 
-use nom::{
-    branch::alt,
-    character::complete::one_of,
-    sequence::{delimited, preceded},
-    IResult,
-};
+use nom::{branch::alt, character::complete::one_of, sequence::delimited, IResult};
 
 use crate::{
     basic_types::{Context, Record},
-    value::{NumericValue, Value},
+    value::Value,
 };
 
 mod binary_math;
+mod field_reference;
 mod literal;
 mod regex_match;
 mod variable;
@@ -22,38 +18,6 @@ pub(crate) trait Expression: Debug {
     fn evaluate<'a>(&self, context: &Context, record: &'a Record) -> Value;
 
     fn regex<'a>(&'a self) -> Option<&'a Regex>;
-}
-
-#[derive(Debug)]
-enum ExpressionImpl {
-    FieldReference(Box<dyn Expression>),
-}
-
-impl Expression for ExpressionImpl {
-    fn regex<'a>(&'a self) -> Option<&'a Regex> {
-        None
-    }
-
-    fn evaluate<'a>(&self, context: &Context, record: &'a Record) -> Value {
-        match self {
-            ExpressionImpl::FieldReference(expression) => {
-                let value = expression.evaluate(context, record).coerce_to_numeric();
-                let unsafe_index = match value {
-                    NumericValue::Integer(i) => i,
-                    NumericValue::Float(f) => f.floor() as i64,
-                };
-                match unsafe_index {
-                    i if i < 0 => panic!("Field indexes cannot be negative: {}", unsafe_index),
-                    i if i == 0 => Value::String(record.full_line.to_string()),
-                    i => record
-                        .fields
-                        .get((i - 1) as usize)
-                        .map(|s| Value::String(s.to_string()))
-                        .unwrap_or(Value::Uninitialized),
-                }
-            }
-        }
-    }
 }
 
 /// Tiers of parsing
@@ -71,7 +35,7 @@ fn parse_primary(input: &str) -> IResult<&str, Box<dyn Expression>> {
         literal::parse_literal,
         variable::parse_variable,
         parse_parens,
-        parse_field_reference,
+        field_reference::parse_field_reference,
     ))(input)
 }
 
@@ -79,15 +43,10 @@ fn parse_parens(input: &str) -> IResult<&str, Box<dyn Expression>> {
     delimited(one_of("("), parse_expression, one_of(")"))(input)
 }
 
-fn parse_field_reference(input: &str) -> IResult<&str, Box<dyn Expression>> {
-    let (i, expr) = preceded(one_of("$"), parse_expression)(input)?;
-    Result::Ok((i, Box::new(ExpressionImpl::FieldReference(expr))))
-}
-
 #[cfg(test)]
 mod tests {
-    use super::literal::*;
     use super::*;
+    use crate::value::NumericValue;
 
     fn empty_context_and_record() -> (Context, Record<'static>) {
         (
@@ -97,18 +56,6 @@ mod tests {
                 fields: vec![],
             },
         )
-    }
-
-    #[test]
-    fn field_reference_can_evaluate() {
-        let (context, mut record) = empty_context_and_record();
-        record.fields = vec!["first", "second"];
-
-        assert_eq!(
-            ExpressionImpl::FieldReference(Box::new(Literal::Numeric(NumericValue::Integer(1))))
-                .evaluate(&context, &record),
-            Value::String("first".to_string()),
-        );
     }
 
     #[test]
@@ -127,21 +74,6 @@ mod tests {
         assert_eq!(
             result.unwrap().1.evaluate(&context, &record),
             Value::Numeric(NumericValue::Float(3.5))
-        );
-    }
-
-    #[test]
-    fn test_parse_field_reference() {
-        let (context, mut record) = empty_context_and_record();
-        let result = parse_expression("$1");
-        assert_eq!(result.is_ok(), true);
-        let expression = result.unwrap().1;
-        assert_eq!(expression.evaluate(&context, &record), Value::Uninitialized,);
-
-        record.fields = vec!["hello"];
-        assert_eq!(
-            expression.evaluate(&context, &record),
-            Value::String("hello".to_string()),
         );
     }
 }
