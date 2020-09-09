d08182b3beff6d54fe2fcc567e81bf507decf433
Wenley Tong <wenley.tong@gmail.com>
2020-09-08 21:27:32 -0700

    Test: More integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -36,6 +36,27 @@ fn check_program_with_input(program_string: &str, input: &str) {
     assert_eq!(rust_output, reference_output);
 }
 
+fn check_program_path_with_input_path(program_path: &str, input_path: &str) {
+    let rust_output = run_command_with_input(
+        Command::new("cargo").args(&[
+            "run",
+            "--bin",
+            "rust-awk",
+            "--",
+            "-f",
+            program_path,
+            input_path,
+        ]),
+        "",
+    );
+    let reference_output = run_command_with_input(
+        Command::new("awk").args(&["-f", program_path, input_path]),
+        "",
+    );
+
+    assert_eq!(rust_output, reference_output);
+}
+
 #[test]
 fn first_test() {
     let program_string = r#"{ print($0); }"#;
@@ -59,4 +80,19 @@ fn accepts_dash_f_to_specify_file() {
         "hello",
     );
     assert_eq!(output, "hello\n");
+}
+
+#[test]
+fn allows_multiple_input_files() {}
+
+#[test]
+fn all_integration_tests() {
+    for directory in std::fs::read_dir("tests/test_cases/").unwrap() {
+        let directory_path = directory.unwrap().path().to_str().unwrap().to_string();
+        let program_path = format!("{}/program.awk", directory_path);
+        for file in std::fs::read_dir(directory_path).unwrap() {
+            let input_path = file.unwrap().path().to_str().unwrap().to_string();
+            check_program_path_with_input_path(&program_path, &input_path);
+        }
+    }
 }
