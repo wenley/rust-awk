586f471857033037ca2117dd72b39b121d3aaf35
Wenley Tong <wenley.tong@gmail.com>
2020-09-11 22:05:27 -0700

    WIP: Consolidate parsing

--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,4 @@
-use nom::{branch::alt, character::complete::multispace0, re_find, sequence::preceded, IResult};
+use nom::{character::complete::multispace0, re_find, sequence::preceded, IResult};
 
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub(crate) enum NumericValue {
@@ -26,11 +26,7 @@ impl Value {
     pub(crate) fn coerce_to_numeric(&self) -> NumericValue {
         match self {
             Value::Numeric(n) => *n,
-            Value::String(s) => match preceded(
-                multispace0,
-                alt((parse_float_literal, parse_integer_literal)),
-            )(s)
-            {
+            Value::String(s) => match preceded(multispace0, parse_numeric)(s) {
                 Ok((_, n)) => n,
                 Err(_) => NumericValue::Float(0.0),
             },
@@ -65,22 +61,10 @@ impl Clone for Value {
 }
 
 pub(crate) fn parse_numeric(input: &str) -> IResult<&str, NumericValue> {
-    alt((parse_float_literal, parse_integer_literal))(input)
-}
-
-fn parse_float_literal(input: &str) -> IResult<&str, NumericValue> {
-    // Omit ? on the . to intentionally _not_ match on integers
-    let (input, matched) = re_find!(input, r"^[-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?")?;
+    let (input, matched) = re_find!(input, r"^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?")?;
     let number = matched.parse::<f64>().unwrap();
 
     IResult::Ok((input, NumericValue::Float(number)))
-}
-
-fn parse_integer_literal(input: &str) -> IResult<&str, NumericValue> {
-    let (input, matched) = re_find!(input, r"^[-+]?[0-9]+")?;
-    let number = matched.parse::<i64>().unwrap();
-
-    IResult::Ok((input, NumericValue::Integer(number)))
 }
 
 #[cfg(test)]
@@ -119,47 +103,44 @@ mod tests {
     #[test]
     fn parse_number_literals() {
         // Integers
+        assert_eq!(parse_numeric("123").unwrap().1, NumericValue::Integer(123));
         assert_eq!(
-            parse_integer_literal("123").unwrap().1,
-            NumericValue::Integer(123)
-        );
-        assert_eq!(
-            parse_integer_literal("123000").unwrap().1,
+            parse_numeric("123000").unwrap().1,
             NumericValue::Integer(123000)
         );
         assert_eq!(
-            parse_integer_literal("-123").unwrap().1,
+            parse_numeric("-123").unwrap().1,
             NumericValue::Integer(-123)
         );
-        assert_eq!(parse_integer_literal("(123").is_err(), true);
+        assert_eq!(parse_numeric("(123").is_err(), true);
         // Would like this test to pass, but the distinction is implemented
         // by the sequencing of the parsers of parse_number_literal
-        // assert_eq!(parse_integer_literal("123.45").is_err(), true);
-        assert_eq!(parse_integer_literal(".").is_err(), true);
+        // assert_eq!(parse_numeric("123.45").is_err(), true);
+        assert_eq!(parse_numeric(".").is_err(), true);
 
         // Floats
         assert_eq!(
-            parse_float_literal("123.45"),
+            parse_numeric("123.45"),
             IResult::Ok(("", NumericValue::Float(123.45)))
         );
         assert_eq!(
-            parse_float_literal("123.45e-5"),
+            parse_numeric("123.45e-5"),
             IResult::Ok(("", NumericValue::Float(123.45e-5)))
         );
         assert_eq!(
-            parse_float_literal("123.45E5"),
+            parse_numeric("123.45E5"),
             IResult::Ok(("", NumericValue::Float(123.45e5)))
         );
         assert_eq!(
-            parse_float_literal(".45"),
+            parse_numeric(".45"),
             IResult::Ok(("", NumericValue::Float(0.45)))
         );
         assert_eq!(
-            parse_float_literal("-123.45"),
+            parse_numeric("-123.45"),
             IResult::Ok(("", NumericValue::Float(-123.45)))
         );
-        assert_eq!(parse_float_literal("a").is_err(), true);
-        assert_eq!(parse_float_literal(".").is_err(), true);
-        assert_eq!(parse_float_literal("+e").is_err(), true);
+        assert_eq!(parse_numeric("a").is_err(), true);
+        assert_eq!(parse_numeric(".").is_err(), true);
+        assert_eq!(parse_numeric("+e").is_err(), true);
     }
 }
